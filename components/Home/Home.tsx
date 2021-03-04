b6b49ff8dd2e5dd961b8ddd5fbe4a47e4d4e5366
Andrew Bliss <andrewgbliss@gmail.com>
2021-03-04 15:17:27 +0900

    updates

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,7 +3,7 @@ import Links from '../Links';
 export default function Home() {
   return (
     <>
-      <div className="full-screen flex justify-center items-center flex-col">
+      <div className="full-screen flex justify-center items-center flex-col p-5">
         <h2 className="text-5xl label">Happy Memories!</h2>
         <div className="pt-10">
           <Links />
